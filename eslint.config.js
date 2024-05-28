b816ba57a3d30780fc104606169fa2b5618658af
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2024-05-28 15:32:56 +0000

    Update ESLint configuration to CommonJS

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-export default [
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
-    rules: {
-      'indent': ['error', 2],
-      'linebreak-style': ['error', 'unix'],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
-    },
+// eslint.config.js
+module.exports = {
+  files: ['**/*.js'],
+  languageOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
   },
-];
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+  },
+};
+  
